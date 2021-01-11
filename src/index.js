7230af6acf86c084f279f6944ef4da7bc8571c82
Morten <mtnielsen89@gmail.com>
2021-01-11 17:20:14 +0100

    Added examples of global settings for axios

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from "axios";
 
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN"; // This is just an example
+axios.defaults.headers.post["Content-Type"] = "application/json"; // This is just an example
+
 axios.interceptors.request.use(request => {
     console.log(request);
     // Edit the request configuration here
